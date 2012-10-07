886c3668069272f45b6348c951474dfbb76d8161
Nick Masters <nmpolo@gmail.com>
2012-10-07 21:40:17 +0000

    Removed localhost check

--- web/app_dev.php
+++ web/app_dev.php
@@ -15,8 +15,8 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
         '::1',
     ))
 ) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+//    header('HTTP/1.0 403 Forbidden');
+//    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
