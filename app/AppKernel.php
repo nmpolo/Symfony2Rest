fdc2af15a04873fe49346a9fd3e16796689137c4
Nick Masters <nmpolo@gmail.com>
2012-10-07 21:41:53 +0000

    FOSRest

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle($this),
+            new FOS\RestBundle\FOSRestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
