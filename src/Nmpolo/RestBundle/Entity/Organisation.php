7d829964e1d5b6fe3b37c88c0d6fbda691de8386
Nick Masters <nmpolo@gmail.com>
2012-10-07 21:53:08 +0000

    Entities

---
+++ src/Nmpolo/RestBundle/Entity/Organisation.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Nmpolo\RestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Nmpolo\RestBundle\Entity\Organisation
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Nmpolo\RestBundle\Entity\OrganisationRepository")
+ */
+class Organisation
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Organisation
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
