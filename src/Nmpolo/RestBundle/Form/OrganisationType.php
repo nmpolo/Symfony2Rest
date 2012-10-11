9e5296e07e038cc6dd7291841bf8f0ec3a1ddc2d
Nick Masters <nmpolo@gmail.com>
2012-10-11 23:03:41 +0000

    Generated forms

---
+++ src/Nmpolo/RestBundle/Form/OrganisationType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Nmpolo\RestBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class OrganisationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Nmpolo\RestBundle\Entity\Organisation'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'nmpolo_restbundle_organisationtype';
+    }
+}
---
+++ src/Nmpolo/RestBundle/Form/UserType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Nmpolo\RestBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('organisation')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Nmpolo\RestBundle\Entity\User'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'nmpolo_restbundle_usertype';
+    }
+}
